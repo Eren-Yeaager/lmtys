3ef52f3ccf5942d6e360f694b5c82328c03bfede
Eren-Yeaager <saswatmarpureddy@gmail.com>
2024-10-11 13:18:50 +0530

    blog ep and middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,46 +1,17 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
-
-app.post("/api/v1/user/signup", async (c) => {
-  const body = await c.req.json();
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-  try {
-    await prisma.user.create({
-      data: {
-        username: body.username,
-        password: body.password,
-        name: body.name,
-      },
-    });
-    return c.text("Signed Up");
-  } catch (e) {
-    c.status(411);
-    return c.text("Invalid");
-  }
-});
-app.post("/api/v1/user/signin", (c) => {
-  return c.text("Hello Hono!");
-});
-app.post("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-app.put("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-app.get("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-app.get("/api/v1/blog/bulk", (c) => {
-  return c.text("Hello Hono!");
-});
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
 export default app;
